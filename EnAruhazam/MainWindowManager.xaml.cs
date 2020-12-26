0394a413194b1269eedbcb6ebccfbda3e41ec838
pingvin12 <fenyesj@gmail.com>
2020-12-26 09:20:10 +0100

    Kezelők(Managerek) hozzáadva

--- EnAruhazam/MainWindowManager.xaml.cs
+++ EnAruhazam/MainWindowManager.xaml.cs
@@ -19,21 +19,37 @@ namespace EnAruhazam
     /// </summary>
     public partial class MainWindowManager : Window
     {
-        public MainWindowManager(string SignedInUser)
+        
+
+
+    public MainWindowManager(string SignedInUser)
         {
             InitializeComponent();
-            UserLoggedInAs.Content = "Bejelentkezve mint: " + SignedInUser;
             
+            UserLoggedInAs.Content = "Bejelentkezve mint: " + SignedInUser;
+            LoadProjected();
+           
+
         }
+
+
+
+       
+
+        //Az ablak bezárásakor meg kell győződnünk, hogy tényleg becsuktuk-e.
         protected override void OnClosed(EventArgs e)
         {
+
             base.OnClosed(e);
             Application.Current.Shutdown();
         }
 
+        
+        public void LoadProjected()
+        {
 
-
-        private void SetToFullScreen(bool tofullscreen)
+        }
+    private void SetToFullScreen(bool tofullscreen)
         {
             if (tofullscreen) {
                 WindowStyle = WindowStyle.None;
@@ -57,6 +73,49 @@ namespace EnAruhazam
         }
 
         private void RichTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void changeWindowChild(Window window)
+        {
+            EM.Visibility = Visibility.Hidden;
+            OM.Visibility = Visibility.Hidden;
+            HR.Visibility = Visibility.Hidden;
+            Back.Visibility = Visibility.Visible;
+            ContentDisplay.Children.Clear();
+
+            object content = window.Content;
+            window.Content = null;
+            window.Close();
+            this.ContentDisplay.Children.Add(content as UIElement);
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            ContentDisplay.Children.Clear();
+
+        }
+
+            private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            PeopleManager peopleManager = new PeopleManager();
+            changeWindowChild(peopleManager);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            EdatManager edatManager = new EdatManager();
+            changeWindowChild(edatManager);
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            OrderManager orderManager = new OrderManager();
+            changeWindowChild(orderManager);
+        }
+
+        private void ProjectedTraffic_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
 
         }
---
+++ EnAruhazam/PeopleManager.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace EnAruhazam
+{
+    /// <summary>
+    /// Interaction logic for PeopleManager.xaml
+    /// </summary>
+    public partial class PeopleManager : Window
+    {
+        public PeopleManager()
+        {
+            InitializeComponent();
+            LoadData();
+
+        }
+
+
+        public void LoadData()
+        {
+            string sqlConnection = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=EnAruhazam;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+
+            string CmdString = string.Empty;
+
+            using (SqlConnection con = new SqlConnection(sqlConnection))
+
+            {
+
+                CmdString = "SELECT Name,DateJoined,Email,Phone FROM dbo.Workers";
+
+                SqlCommand cmd = new SqlCommand(CmdString, con);
+
+                SqlDataAdapter sda = new SqlDataAdapter(cmd);
+
+                DataTable dt = new DataTable("Worker");
+
+                sda.Fill(dt);
+
+                PeopleGrid.ItemsSource = dt.DefaultView;
+            }
+            }
+
+        private void PeopleGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void GoBack_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
